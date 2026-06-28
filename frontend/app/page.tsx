68d5bb65caf3147fe4e7ac44225f876794af4155
Krushna Ganesh Pokharkar <160628406+Krushna2142@users.noreply.github.com>
2026-06-28 23:39:50 +0530

    chore: deprecate old home page location
    - File moved to app/(marketing)/page.tsx
    - Uses route groups for better organization
    - This file will be removed in cleanup

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,19 +1,2 @@
-import { HeroSection } from "@/src/components/sections/HeroSection";
-import { QuickLinks } from "@/src/components/sections/QuickLinks";
-import { ImageSlider } from "@/src/components/sections/ImageSlider";
-import { SponsorsSection } from "@/src/components/sections/SponsorsSection";
-import { CommitteeMembers } from "@/src/components/sections/CommitteeMembers";
-import { NewsletterCard } from "@/src/components/sections/NewsletterCard";
-
-export default function Home() {
-  return (
-    <>
-      <HeroSection />
-      <QuickLinks />
-      <SponsorsSection />
-       <ImageSlider />
-      <CommitteeMembers />
-      <NewsletterCard />
-    </>
-  );
-}
+// This file has been moved to: app/(marketing)/page.tsx
+// Please update your imports accordingly
